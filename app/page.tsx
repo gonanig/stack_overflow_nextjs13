9f7e18d5abf60b9887737f5ac152ac84bc226350
ahoncharova <ahoncharova@telecard.kiev.ua>
2024-08-18 18:11:44 +0300

    setup eslint, prettier, settings json

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-	return (
-		<div>
-			<h1 className='h1-bold'>Hello</h1>
-		</div>
-	)
+  return (
+    <div>
+      <h1 className="h1-bold">Hello</h1>
+    </div>
+  );
 }
