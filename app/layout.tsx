4db1c43a2053b572b09e5869b6b5c2f4584af8f8
ahoncharova <ahoncharova@telecard.kiev.ua>
2024-08-21 19:48:41 +0300

    add assets amd layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   ClerkProvider,
   SignInButton,
@@ -5,23 +6,48 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
-import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structure and more.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold"> This is piece of text</h1>
           {children}
         </body>
       </html>
